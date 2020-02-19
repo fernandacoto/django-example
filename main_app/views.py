feeaf88fa7ff5f4fdac8e8a720455c1ca96b0246
Fernanda Coto <fernanda.coto@lognllc.com>
2020-02-19 09:49:53 -0600

    Create Model and Query treasures from DB

--- main_app/views.py
+++ main_app/views.py
@@ -2,22 +2,9 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from .models import Treasure
 
 
 def index(request):
-    return render(request, 'index.html', {'treasures': trasures})
-
-
-class Treasure:
-    def __init__(self, name, value, material, location):
-        self.name = name
-        self.value = value
-        self.material = material
-        self.location = location
-
-
-trasures = [
-    Treasure('Gold Nugget', 500.00, 'gold', "Curlys's Creek, NM"),
-    Treasure("Fool's Gold", 0, 'pyrite', "Fool's Falls, CO"),
-    Treasure('Coffee Can', 20.00, 'tin', "Acme, CA")
-]
+    treasures = Treasure.objects.all()
+    return render(request, 'index.html', {'treasures': treasures})
